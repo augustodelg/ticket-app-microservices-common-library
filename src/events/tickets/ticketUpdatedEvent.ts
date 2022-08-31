a541974f9f72185ed53434ec138d325bfc9ee397
Augusto Del Grosso <adg.prodlp@gmail.com>
2022-08-31 10:45:24 -0300

    Updates

--- src/events/tickets/ticketUpdatedEvent.ts
+++ src/events/tickets/ticketUpdatedEvent.ts
@@ -5,6 +5,7 @@ export interface TicketUpdatedEvent {
     subject: Subjects.TicketUpdated;
     data: {
         id: string;
+        version: number;
         title: string;
         price: number;
         userId: string;
